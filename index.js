ea4228d1b6fbdd70c240e69aa82675dd49e3f001
rusty619 <rusty6916@gmail.com>
2022-03-17 14:25:56 -0400

    commit tree a84f57843b824e52e76f728aa3c9995b56dcc618

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+                                                                                                                                                                                                                                                       0// Code your solutions in this file
+let writeCards = (arr) => {
+    var newArr = []
+    for(var i = 0; i < arr.length; i++){
+        newArr[i] = `Thank you, ${arr[i]}, for the wonderful surprise gift!`
+    }
+    return newArr
+}
+
+let countDown = (num) => {
+    for(let i = num; i >= 0; i--){
+        console.log(i)
+    }
+}
+
+console.log(countDown())
